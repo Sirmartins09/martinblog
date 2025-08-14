d271a35ad47cb8ec246bd7b656e7349cef273709
Martin Dean <martindn6@gmail.com>
2025-08-14 00:42:39 -0300

    cambio en los botones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 document.getElementById('hamburger').addEventListener('click', function () {
     document.getElementById('nav-links').classList.toggle('show');
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const likeButton = document.querySelector(".like-button");
+    const likeCount = document.getElementById("like-count");
+    let count = 0;
+
+    likeButton.addEventListener("click", (event) => {
+        event.preventDefault(); // Evita que el enlace <a> se active
+        count++;
+        likeCount.textContent = count;
+    });
+});
